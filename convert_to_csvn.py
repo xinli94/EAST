a872caccb691498743ce1adb518a40711403e678
Xin Li <xinli.94@hotmail.com>
2019-02-27 17:09:04 -0800

    Data format converter: eval output to csvn i.e. pr script compatible

---
+++ convert_to_csvn.py
@@ -0,0 +1,36 @@
+import argparse
+import glob
+import numpy as np
+import os
+import pandas as pd
+
+# # # input_folder = '/data5/xin/ocr/output'
+# input_folder = 'output'
+# ground_truth = False
+# output_file = '/data5/xin/ocr/data_csv/result.csv'
+
+input_folder = '/data5/xin/ocr/rot_boxes_v2_class/boxes/'
+ground_truth = True
+output_file = '/data5/xin/ocr/data_csv/groundtruth.csv'
+
+files = glob.glob(os.path.join(input_folder, '*.txt'))
+records = []
+for file in files:
+    image_name = os.path.splitext(os.path.basename(file))[0] + '.jpeg'
+
+    data = np.genfromtxt(file, delimiter=',')
+    for idx, item in enumerate(data):
+        box = item[:8].reshape(4,2)
+        min_h, max_h = box[:,1].min(), box[:,1].max()
+        min_w, max_w = box[:,0].min(), box[:,0].max()
+        if ground_truth:
+            # path,image_width,image_height,left,top,right,bottom,label
+            records.append([image_name, -1, -1, min_w, min_h, max_w, max_h, 'text'])
+        else:
+            assert len(item) == 9
+            score = item[-1]
+            # path,timestamp,image_width,image_height,left,top,right,bottom,score,label
+            records.append([image_name, -1, -1, -1, min_w, min_h, max_w, max_h, score, 'text'])
+
+df = pd.DataFrame.from_records(records)
+df.to_csv(output_file, header=None, index=None)
