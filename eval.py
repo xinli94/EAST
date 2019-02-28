de84f8be442f334d5e8c4b16f0bb4c6dcd8a6feb
Xin Li <xinli.94@hotmail.com>
2019-02-27 17:10:33 -0800

    Modify eval to output boxes with score

--- eval.py
+++ eval.py
@@ -13,6 +13,8 @@ tf.app.flags.DEFINE_string('gpu_list', '0', '')
 tf.app.flags.DEFINE_string('checkpoint_path', '/tmp/east_icdar2015_resnet_v1_50_rbox/', '')
 tf.app.flags.DEFINE_string('output_dir', '/tmp/ch4_test_images/images/', '')
 tf.app.flags.DEFINE_bool('no_write_images', False, 'do not write images')
+tf.app.flags.DEFINE_float('score_threshold', 0.8, 'score threshold to use')
+tf.app.flags.DEFINE_float('vis_score_threshold', 0.8, 'score threshold to use')
 
 import model
 from icdar import restore_rectangle
@@ -85,7 +87,11 @@ def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_
     # filter the score map
     xy_text = np.argwhere(score_map > score_map_thresh)
     # sort the text boxes via the y axis
-    xy_text = xy_text[np.argsort(xy_text[:, 0])]
+    mask = np.argsort(xy_text[:, 0])
+    xy_text = xy_text[mask]
+    # scores
+    scores_filtered = score_map[score_map > score_map_thresh]
+    scores_filtered = scores_filtered[mask]
     # restore
     start = time.time()
     text_box_restored = restore_rectangle(xy_text[:, ::-1]*4, geo_map[xy_text[:, 0], xy_text[:, 1], :]) # N*4*2
@@ -110,7 +116,7 @@ def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_
         boxes[i, 8] = cv2.mean(score_map, mask)[0]
     boxes = boxes[boxes[:, 8] > box_thresh]
 
-    return boxes, timer
+    return boxes, timer, scores_filtered
 
 
 def sort_poly(p):
@@ -149,7 +155,8 @@ def main(argv=None):
             saver.restore(sess, model_path)
 
             im_fn_list = get_images()
-            for im_fn in im_fn_list:
+            total = len(im_fn_list)
+            for idx, im_fn in enumerate(im_fn_list):
                 im = cv2.imread(im_fn)[:, :, ::-1]
                 start_time = time.time()
                 im_resized, (ratio_h, ratio_w) = resize_image(im)
@@ -159,9 +166,15 @@ def main(argv=None):
                 score, geometry = sess.run([f_score, f_geometry], feed_dict={input_images: [im_resized]})
                 timer['net'] = time.time() - start
 
-                boxes, timer = detect(score_map=score, geo_map=geometry, timer=timer)
-                print('{} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
-                    im_fn, timer['net']*1000, timer['restore']*1000, timer['nms']*1000))
+                boxes, timer, scores_filtered = detect(
+                    score_map=score,
+                    geo_map=geometry,
+                    timer=timer,
+                    score_map_thresh=FLAGS.score_threshold
+                )
+
+                print('[{} / {} ({}%)] {} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
+                    idx+1, total, round(100.0 * (idx+1) / total, 2), im_fn, timer['net']*1000, timer['restore']*1000, timer['nms']*1000))
 
                 if boxes is not None:
                     boxes = boxes[:, :8].reshape((-1, 4, 2))
@@ -179,15 +192,16 @@ def main(argv=None):
                             os.path.basename(im_fn).split('.')[0]))
 
                     with open(res_file, 'w') as f:
-                        for box in boxes:
+                        for box, cur_score in zip(boxes, scores_filtered):
                             # to avoid submitting errors
                             box = sort_poly(box.astype(np.int32))
                             if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3]-box[0]) < 5:
                                 continue
-                            f.write('{},{},{},{},{},{},{},{}\r\n'.format(
-                                box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1],
+                            f.write('{},{},{},{},{},{},{},{},{}\r\n'.format(
+                                box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1], cur_score
                             ))
-                            cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
+                            if cur_score > FLAGS.vis_score_threshold:
+                                cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
                 if not FLAGS.no_write_images:
                     img_path = os.path.join(FLAGS.output_dir, os.path.basename(im_fn))
                     cv2.imwrite(img_path, im[:, :, ::-1])
